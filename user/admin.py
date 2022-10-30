098265a9519fa12edaa21b03ac620499cbedc548
Milan Sony <milanfrom2001@gmail.com>
2022-10-30 13:54:00 +0530

    user model created and added to adminside

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from user.models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    """Define admin model for custom User model with no username field."""
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal details'), {'fields': ('name',)}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('name', 'email', 'is_active')
+    search_fields = ('email', 'name')
+    ordering = ('email',) # Without the comma, ('email') is the same as 'email', which is a string, not a tuple.
+    # https://stackoverflow.com/questions/31765584/django-admin-error-admin-e008-the-value-of-fieldsets-must-be-a-list-or-tuple
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from datetime import datetime
+from .managers import CustomUserManager
 
 # Create your models here.
+
+#! Creating custom user model with AbstractUser
+# Reference link about AbstractUser:
+# https://testdriven.io/blog/django-custom-user-model/
+# https://stackoverflow.com/questions/21514354/difference-between-abstractuser-and-abstractbaseuser-in-django
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+  name = models.CharField(max_length=150)
+  email = models.EmailField(unique=True)
+  date_joined = models.DateTimeField(default=datetime.now())
+  is_staff = models.BooleanField(default=False)
+  is_active = models.BooleanField(default=True)
+  is_superuser = models.BooleanField(default=False)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+
+  objects = CustomUserManager()
+
+  def __str__(self):
+    return self.email
+  # This is used to change the adminside object name
+
+  class Meta:
+    verbose_name = "user"
+    verbose_name_plural = "users"
+    db_table = 'accounts'
+  #   abstract = True
+  # In this case, (inside AbstractBaseUser) it is defined as abstract = True
+  # If abstract = True, this model will be an abstract  base class
